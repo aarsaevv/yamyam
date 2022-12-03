55bf60430562181793d37cdd46b76773dcd57c39
Anna Kautts <annakautts@gmail.com>
2022-12-04 03:39:25 +0600

    Menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -13,6 +13,110 @@
           <a href=""><img src="../assets/cart.svg" alt="Cart" /></a>
         </div>
       </div>
+      <div class="menu">
+        <div class="menu__navigation">
+          <a href="#">ramen 拉麺</a>
+          <a href="#">soup スープ</a>
+          <a href="#">rice ご飯</a>
+          <a href="#">salad サラダ</a>
+        </div>
+        <div class="menu__list">
+          <div class="menu__list__item">
+            <div class="menu__list__item__card">
+              <h3>Miso ramen</h3>
+              <img src="../assets/ramen-menu.svg" alt="Ramen" />
+              <p>
+                Coming from the Sapporo region of Hokkaido, miso ramen takes its
+                name from its main ingredient. This broth is strong and savory
+                and has an opaque appearance.
+              </p>
+              <div class="card-price">
+                <h5>12$ / 500gr</h5>
+                <button class="card-button">
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="menu__list__item">
+            <div class="menu__list__item__card">
+              <h3>Tonkotsu ramen</h3>
+              <img src="../assets/ramen-menu.svg" alt="Ramen" />
+              <p>
+                Originating from modern-day Fukuoka and lending its mouthwatering fragrance to Tokyo’s Asakusa region, tonkotsu ramen is made from boiling pork.
+              </p>
+              <div class="card-price">
+                <h5>15$ / 500gr</h5>
+                <button class="card-button">
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="menu__list__item">
+            <div class="menu__list__item__card">
+              <h3>Shoyu Ramen</h3>
+              <img src="../assets/ramen-menu.svg" alt="Ramen" />
+              <p>
+                Shoyu means soy sauce in Japanese and this style of noodle dish was actually the first type of ramen and is still going strong.
+              </p>
+              <div class="card-price">
+                <h5>10$ / 500gr</h5>
+                <button class="card-button">
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="menu__list">
+          <div class="menu__list__item">
+            <div class="menu__list__item__card">
+              <h3>shio ramen</h3>
+              <img src="../assets/ramen-menu.svg" alt="Ramen" />
+              <p>
+                Shio means salt and this style of ramen tends to be light and transparent. It’s often made by boiling down chicken bones and flavored with seafood.
+              </p>
+              <div class="card-price">
+                <h5>10$ / 500gr</h5>
+                <button class="card-button">
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="menu__list__item">
+            <div class="menu__list__item__card">
+              <h3>Tsukemen ramen</h3>
+              <img src="../assets/ramen-menu.svg" alt="Ramen" />
+              <p>
+                These thick and hearty ramen noodles are cooked, plunged into cool water, and then served alongside a bowl of tare ramen broth.
+              </p>
+              <div class="card-price">
+                <h5>18$ / 500gr</h5>
+                <button class="card-button">
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="menu__list__item">
+            <div class="menu__list__item__card">
+              <h3>Wakayama ramen</h3>
+              <img src="../assets/ramen-menu.svg" alt="Ramen" />
+              <p>
+                From Shikoku Island, Wakayama Ramen uses a blend of tonkotsu and shoyu style broths to create something spectacular.
+              </p>
+              <div class="card-price">
+                <h5>14$ / 500gr</h5>
+                <button class="card-button">
+                  Add to cart
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -53,113 +157,84 @@ export default {
   color: #bf3c30;
 }
 
-.main-block {
+.menu__navigation {
   display: flex;
   justify-content: space-between;
-}
-
-.main-block__text {
-  width: 500px;
-  height: 548px;
-  margin: 100px 0 0;
-}
-
-.main-block__text h1 {
-  font-size: 58px;
-  color: #bf3c30;
+  margin: 30px 0 0 0;
+  font-size: 37px;
   font-weight: 900;
-  margin: 0;
 }
 
-.main-block__text img {
-  margin: 35px 0;
-}
-
-.main-block__text h2 {
-  font-size: 38px;
-  font-weight: 900;
-  text-transform: uppercase;
-  margin: 0;
-}
-
-.main-block__text h4 {
-  font-size: 20px;
-  font-weight: 400;
-  margin: 20px 0;
-}
-
-h2 span {
-  color: #bf3c30;
-}
-
-.main-block__button {
-  position: relative;
-  margin: 0 0 0 10px;
-  width: 240px;
-  height: 70px;
-  background-color: #bf3c30;
-  color: #e3dcc3;
-  font-size: 18px;
-  font-weight: 700;
-  letter-spacing: 1px;
-  text-transform: uppercase;
-  border: none;
-  border-radius: 7px;
-}
-
-.button__outline {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  border: 2px solid #262524;
-  border-radius: 7px;
-  top: 7px;
-  left: -10px;
-  z-index: -1;
-}
-
-.button__text {
-  position: absolute;
-  margin-left: auto;
-  margin-right: auto;
-  top: 35%;
-  left: 0;
-  right: 0;
-  text-align: center;
-}
-
-.main-block__image img {
-  margin: 65px 0;
-}
-
-.footer {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 70px;
-  margin: 70px 0 0 0;
-}
-
-.footer__buttons {
-  display: flex;
-  gap: 46px;
-  align-items: center;
-  font-size: 18px;
-  letter-spacing: 1px;
-}
-
-.footer__buttons a {
-  text-decoration: none;
+.menu__navigation a {
   color: #262524;
-  transition: 0.2s all;
+  text-decoration: none;
+  text-transform: uppercase;
 }
 
-.footer__buttons :hover {
-  color: #bf3c30;
+.menu__list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 79px;
+  margin: 30px 0 0 0;
 }
 
-.footer__line {
+.menu__list__item {
   display: flex;
   justify-content: center;
+  align-items: center;
+  width: 262px;
+  height: 341px;
+  border: 3px solid #262524;
+  border-radius: 7px;
+}
+
+.menu__list__item__card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  width: 238px;
+  height: 310px;
+  border: 3px solid #bf3c30;
+  border-radius: 7px;
+}
+
+.menu__list__item__card h3 {
+  margin: 0 auto;
+  font-size: 23px;
+  text-transform: uppercase;
+}
+
+.menu__list__item__card p {
+  height: 90px;
+  letter-spacing: -1px;
+  line-height: 18px;
+  font-size: 15px;
+  text-align: justify;
+  text-indent: 15px;
+  margin: 0 7px;
+}
+
+.card-button {
+  width: 105px;
+  height: 33px;
+  color: #e3dcc3;
+  background-color: #bf3c30;
+  border: none;
+  border-radius: 7px;
+  font-size: 14px;
+}
+
+.card-price {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+}
+
+.card-price h5 {
+  font-size: 15px;
+  margin: 0 auto;
 }
 </style>
