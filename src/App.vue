90b70963717521ff6965c56e3876a5b751df415d
aarsaevv <arsaevgeny@gmail.com>
2022-12-08 01:49:00 +0600

    Deployment at an early stage

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,43 @@
 <template>
-  <!-- <nav>
+	<!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/menu">Menu</router-link> |
     <router-link to="/our-story">Our Story</router-link> |
     <router-link to="/contacts">Contacts</router-link> |
     <router-link to="/cart">Cart</router-link> |
   </nav> -->
-  <router-view />
+	<router-view />
 </template>
 
 <style>
-body {
-  background-color: #f1e2d2;
-  font-family: "Noto Sans", sans-serif;
-  font-weight: 400;
-  color: #262524;
-  margin: 0;
-  padding: 0;
-  height: 100%;
-}
+	body {
+		background-color: #f1e2d2;
+		font-family: "Noto Sans", sans-serif;
+		font-weight: 400;
+		color: #262524;
+		margin: 0;
+		padding: 0;
+		height: 100%;
+	}
 </style>
+
+<script>
+	import { initializeApp } from "firebase/app";
+	import { getFirestore } from "firebase/firestore";
+
+	const firebaseConfig = {
+		apiKey: "AIzaSyDdB-6O1Gd--6xtM9wl9quory9qushT5VI",
+		authDomain: "yamyam-f7b3c.firebaseapp.com",
+		projectId: "yamyam-f7b3c",
+		storageBucket: "yamyam-f7b3c.appspot.com",
+		messagingSenderId: "1038634120582",
+		appId: "1:1038634120582:web:ab504e8d7dfe892e3da807",
+	};
+
+	const app = initializeApp(firebaseConfig);
+	const db = getFirestore(app);
+
+  export default {
+    name: "App",
+};
+</script>
