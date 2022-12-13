4e07747d1e3537f0afa3b1de74cf09087da8fe80
Anna Kautts <annakautts@gmail.com>
2022-12-14 02:09:16 +0600

    Cart check

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -92,18 +92,102 @@
         </div>
         <h2>delivery 配達</h2>
         <div class="delivery-data">
-          <form action="delivery-data__form form">
-            <form class="form__name" action="">
-              <div class="form__name__first-name">
+          <div class="delivery-data__credentials credentials">
+            <div class="credentials__name name">
+              <form class="name__first" action="">
                 <label for="">First name</label>
                 <input type="text" />
-              </div>
-              <div class="form__name__last-name">
+              </form>
+              <form class="name__last" action="">
                 <label for="">Last name</label>
                 <input type="text" />
-              </div>
+              </form>
+            </div>
+            <form action="">
+              <label for="">Email address</label>
+              <input type="email" />
             </form>
-          </form>
+            <form action="">
+              <label for="">Phone number</label>
+              <input type="number" />
+            </form>
+          </div>
+          <div class="delivery-data__address">
+            <form action="">
+              <label for="">Address line 1</label>
+              <input type="text" />
+            </form>
+            <form action="">
+              <label for="">Address line 2</label>
+              <input type="text" />
+            </form>
+          </div>
+          <div class="delivery-data__payment payment">
+            <form action="">
+              <label for="">Cardholder name</label>
+              <input type="text" />
+            </form>
+            <form action="">
+              <label for="">Card number</label>
+              <input type="text" />
+            </form>
+            <div class="payment__info info">
+              <form class="info__card-expired" action="">
+                <label for="">Card expired</label>
+                <input type="text" />
+              </form>
+              <form class="info__cvv" action="">
+                <label for="">CVV/CVC</label>
+                <input type="text" />
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="check">
+          <h3>Shopping cart</h3>
+          <div class="check__image">
+            <img src="../assets/mini-line.svg" alt="Line" />
+          </div>
+          <div class="check__delivery delivery">
+            <div class="delivery__item">
+              <h5>Miso ramen</h5>
+              <h5>12$</h5>
+            </div>
+            <div class="delivery__item">
+              <h5>Rice with shrimp</h5>
+              <h5>15$</h5>
+            </div>
+            <div class="delivery__item">
+              <h5>Wakame salad</h5>
+              <h5>10$</h5>
+            </div>
+          </div>
+          <div class="check__image">
+            <img src="../assets/mini-line.svg" alt="Line" />
+          </div>
+          <div class="check__subtotal subtotal">
+            <div class="subtotal__item">
+              <h5>Subtotal</h5>
+              <h5>37$</h5>
+            </div>
+            <div class="subtotal__item">
+              <h5>Tax</h5>
+              <h5>2.22$$</h5>
+            </div>
+            <div class="subtotal__item">
+              <h5>Delivery</h5>
+              <h5>8$</h5>
+            </div>
+            <div class="check__image">
+              <img src="../assets/mini-line.svg" alt="Line" />
+            </div>
+            <div class="check__total total">
+              <div class="total__item">
+                <h5>Total</h5>
+                <h5>47.22$</h5>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -122,7 +206,7 @@ export default {
 .cart h2 {
   display: flex;
   justify-content: center;
-  margin: 30px 0;
+  margin: 45px 0;
   height: 60px;
   color: #262524;
   font-size: 37px;
@@ -211,25 +295,149 @@ export default {
   margin: 25px 0 30px 0;
 }
 
-.form__name__first-name label {
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+}
+
+form label {
   font-size: 20px;
   font-weight: 700;
   text-transform: uppercase;
 }
 
-.form__name__last-name label {
+form input {
+  width: 483px;
+  height: 26px;
+  background-color: rgba(255, 255, 255, 0.7);
+  border: 2px solid rgba(37, 38, 36, 0.3);
+  border-radius: 7px;
+  padding: 0;
+  margin: 0;
+}
+
+.delivery-data {
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+  width: 487px;
+}
+
+.delivery-data__credentials {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.name__first input,
+.name__last input {
+  width: 221px;
+}
+
+.credentials__name {
+  display: flex;
+  gap: 37px;
+}
+
+.delivery-data__address {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.delivery-data__payment {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.payment__info {
+  display: flex;
+  gap: 37px;
+}
+
+.info__card-expired input,
+.info__cvv input {
+  width: 221px;
+}
+
+.check {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 360px;
+  height: 457px;
+  background-color: rgba(255, 255, 255, 0.7);
+  border: 2px solid rgba(37, 38, 36, 0.3);
+  border-radius: 7px;
+}
+
+.check h3 {
+  display: flex;
+  justify-content: center;
+  font-size: 30px;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin: 0;
+}
+
+.check__image {
+  display: flex;
+  justify-content: center;
+  margin: 10px 0;
+}
+
+.check__delivery {
+  width: 300px;
+  height: 180px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin: 0 auto;
+}
+
+.delivery__item {
+  display: flex;
+  justify-content: space-between;
+}
+
+.check__subtotal {
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
+
+.subtotal__item {
+  display: flex;
+  justify-content: space-between;
+}
+
+.total__item {
+  display: flex;
+  justify-content: space-between;
+}
+
+.delivery__item h5 {
+  font-size: 20px;
+  font-weight: 400;
+  margin: 0;
+}
+
+.subtotal__item h5 {
+  display: flex;
   font-size: 20px;
   font-weight: 700;
   text-transform: uppercase;
+  margin: 0;
 }
 
-.form__name__first-name input {
-  width: 225px;
-  height: 25px;
-}
-
-.form__name__last-name input {
-  width: 225px;
-  height: 25px;
+.total__item h5 {
+  display: flex;
+  font-size: 30px;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin: 0;
 }
 </style>
